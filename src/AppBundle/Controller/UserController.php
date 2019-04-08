10064072ec59c9ce291146e5de150551b6649d68
unknown <rishardcarranza@gmail.com>
2019-04-08 16:38:37 -0600

    Added JWT authentication, login verification

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,8 +8,17 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Yaml\Tests\A;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class UserController
-{
+class UserController extends Controller {
 
+    public function newAction() {
+
+    }
 }
